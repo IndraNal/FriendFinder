529bf085079bbd5dce4577aa554405a9fda20f8d
Indra Nallappan <back2indra@gmail.com>
2019-03-08 20:17:09 -0600

    friendFinderapp3

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -25,11 +25,11 @@ require("./routes/htmlRoutes")(app);
 
 
 
-  
 
 
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
 
-  
+app.listen(PORT, function () {
+  console.log("App listening on PORT " + PORT);
+});
+
+
